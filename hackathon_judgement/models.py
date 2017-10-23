727190bfee905edbc2bc123f62dab29237f96c92
James Pettit <etherealmachine@gmail.com>
2017-10-23 20:28:39 +0200

    Add failing test that calculating the winner and assigning points works.

--- hackathon_judgement/models.py
+++ hackathon_judgement/models.py
@@ -2,6 +2,7 @@
 from otree.api import models
 from otree.constants import BaseConstants
 from otree.models import BaseGroup, BasePlayer, BaseSubsession
+from collections import defaultdict
 
 doc = """
 """
@@ -23,7 +24,17 @@ class Subsession(BaseSubsession):
 
 
 class Group(BaseGroup):
-    pass
+    best_overall = models.CharField(max_length=100, choices=Constants.project_names)
+    best_design = models.CharField(max_length=100, choices=Constants.project_names)
+    most_original = models.CharField(max_length=100, choices=Constants.project_names)
+
+    def calculate_winners(self):
+        votes = defaultdict(lambda: [])
+        for player in self.get_players():
+            for category in ['best_overall', 'best_design', 'most_original']:
+                for rank in [1, 2, 3]:
+                    vote = getattr(player, '{}_{}'.format(category, rank))
+                    votes[player].append(vote)
 
 
 class Player(BasePlayer):
@@ -38,4 +49,15 @@ class Player(BasePlayer):
     most_original_3 = models.CharField(max_length=100, choices=Constants.project_names)
 
     def set_payoff(self):
-        pass
+        if self.id_in_group == 1:
+            self.group.calculate_winners()
+
+        self.payoff = 0
+        for category in ['best_overall', 'best_design', 'most_original']:
+            winner = getattr(self.group, category)
+            if getattr(self, '{}_1'.format(category)) == winner:
+                self.payoff += 3
+            if getattr(self, '{}_2'.format(category)) == winner:
+                self.payoff += 2
+            if getattr(self, '{}_3'.format(category)) == winner:
+                self.payoff += 1
